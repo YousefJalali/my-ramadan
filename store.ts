1fc53404e3d3553f36116b9bee72fe8d35e5a142
Yousef Jalali <yousefjalalidev@gmail.com>
2025-02-17 23:45:26 +0300

    fix error

--- store.ts
+++ store.ts
@@ -7,6 +7,8 @@ export const session$ = observable<Session | null>(null)
 const getNormalizedLocale = () => {
   const locale = Localization.getLocales()[0]?.languageTag
 
+  if (!locale) return 'en-US'
+
   if (locale.startsWith('en')) return 'en-US'
   if (locale.startsWith('ar')) return 'ar-SA'
 
