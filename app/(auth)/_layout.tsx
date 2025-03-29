af20621574e8362129a26cdd0026eaca55687ba2
Yousef Jalali <yousefjalalidev@gmail.com>
2025-03-29 16:36:34 +0300

    fix: update layout spacing and add new password translation strings

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -15,7 +15,7 @@ export default function AuthLayout() {
 
   return (
     <VStack className='flex-1 p-6 bg-background-50'>
-      <HStack className='mt-12'>
+      <HStack className='my-12'>
         <GoBackBtn />
       </HStack>
       <Center className='flex-1'>
--- app/(auth)/update-password.tsx
+++ app/(auth)/update-password.tsx
@@ -1,6 +1,4 @@
-import { useState } from 'react'
-import { Toast, ToastTitle, useToast } from '@/components/ui/toast'
-import { HStack } from '@/components/ui/hstack'
+import { useEffect, useState } from 'react'
 import { VStack } from '@/components/ui/vstack'
 import { Heading } from '@/components/ui/heading'
 import { Text } from '@/components/ui/text'
@@ -15,16 +13,15 @@ import {
 } from '@/components/ui/form-control'
 import { Input, InputField, InputIcon, InputSlot } from '@/components/ui/input'
 import { EyeIcon, EyeOffIcon } from '@/components/ui/icon'
-import { Button, ButtonText } from '@/components/ui/button'
+import { Button, ButtonSpinner, ButtonText } from '@/components/ui/button'
 import { Keyboard } from 'react-native'
 import { useForm, Controller } from 'react-hook-form'
 import { z } from 'zod'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { supabase } from '@/utils/supabase'
-import { Link } from 'expo-router'
 import { AlertTriangle } from 'lucide-react-native'
 import { useTranslation } from 'react-i18next'
-import { LinkText } from '@/components/ui/link'
+import { Link, useRouter } from 'expo-router'
 
 const schema = z.object({
   password: z
@@ -51,6 +48,16 @@ const schema = z.object({
 type SchemaType = z.infer<typeof schema>
 
 export default function SignUp() {
+  const [showPassword, setShowPassword] = useState(false)
+  const [showConfirmPassword, setShowConfirmPassword] = useState(false)
+  const [status, setStatus] = useState<null | {
+    status: string
+    message: string
+  }>(null)
+  const [loading, setLoading] = useState(false)
+
+  const router = useRouter()
+
   const { t } = useTranslation()
 
   const {
@@ -62,13 +69,32 @@ export default function SignUp() {
     resolver: zodResolver(schema),
   })
 
+  useEffect(() => {
+    supabase.auth.onAuthStateChange(async (event, session) => {
+      if (event !== 'PASSWORD_RECOVERY') {
+        router.push('/(auth)/reset-password')
+      }
+    })
+  }, [])
+
   const onSubmit = async ({ password }: SchemaType) => {
+    setLoading(true)
     const { data, error } = await supabase.auth.updateUser({
       password,
     })
+    setLoading(false)
+
+    if (data)
+      setStatus({
+        status: 'success',
+        message: 'Password updated successfully!',
+      })
+    if (error)
+      setStatus({
+        status: 'failed',
+        message: 'There was an error updating your password.',
+      })
   }
-  const [showPassword, setShowPassword] = useState(false)
-  const [showConfirmPassword, setShowConfirmPassword] = useState(false)
 
   const handleState = () => {
     setShowPassword((showState) => {
@@ -86,138 +112,141 @@ export default function SignUp() {
   }
 
   return (
-    <VStack className='max-w-[440px] w-full' space='md'>
+    <VStack className='w-full flex-1' space='md'>
       <VStack className='mb-6' space='sm'>
         <Heading className='md:text-center' size='3xl'>
-          {t('sign up')}
+          {t('enter new password')}
         </Heading>
-        <Text>{t('sign up to start using the app')}</Text>
+        <Text>{t('set a new password to secure your account')}</Text>
       </VStack>
 
-      <VStack className='w-full'>
-        <VStack space='xl' className='w-full'>
-          <FormControl isInvalid={!!errors.password}>
-            <FormControlLabel>
-              <FormControlLabelText>{t('password')}</FormControlLabelText>
-            </FormControlLabel>
-            <Controller
-              defaultValue=''
-              name='password'
-              control={control}
-              rules={{
-                validate: async (value) => {
-                  try {
-                    await schema.parseAsync({
-                      password: value,
-                    })
-                    return true
-                  } catch (error: any) {
-                    return error.message
-                  }
-                },
-              }}
-              render={({ field: { onChange, onBlur, value } }) => (
-                <Input>
-                  <InputField
-                    className='text-sm'
-                    placeholder={t('type here...')}
-                    value={value}
-                    onChangeText={onChange}
-                    onBlur={onBlur}
-                    onSubmitEditing={handleKeyPress}
-                    returnKeyType='done'
-                    type={showPassword ? 'text' : 'password'}
-                  />
-                  <InputSlot onPress={handleState} className='px-3'>
-                    <InputIcon as={showPassword ? EyeIcon : EyeOffIcon} />
-                  </InputSlot>
-                </Input>
-              )}
-            />
-            <FormControlError>
-              <FormControlErrorIcon size='sm' as={AlertTriangle} />
-              <FormControlErrorText>
-                {t(errors?.password?.message || '')}
-              </FormControlErrorText>
-            </FormControlError>
-          </FormControl>
-          <FormControl isInvalid={!!errors.confirmPassword}>
-            <FormControlLabel>
-              <FormControlLabelText>
-                {t('confirm password')}
-              </FormControlLabelText>
-            </FormControlLabel>
-            <Controller
-              defaultValue=''
-              name='confirmPassword'
-              control={control}
-              rules={{
-                validate: async (value) => {
-                  try {
-                    await schema.parseAsync({
-                      password: value,
-                    })
-                    return true
-                  } catch (error: any) {
-                    return error.message
-                  }
-                },
-              }}
-              render={({ field: { onChange, onBlur, value } }) => (
-                <Input>
-                  <InputField
-                    placeholder={t('type here...')}
-                    className='text-sm'
-                    value={value}
-                    onChangeText={onChange}
-                    onBlur={onBlur}
-                    onSubmitEditing={handleKeyPress}
-                    returnKeyType='done'
-                    type={showConfirmPassword ? 'text' : 'password'}
-                  />
-
-                  <InputSlot onPress={handleConfirmPwState} className='px-3'>
-                    <InputIcon
-                      as={showConfirmPassword ? EyeIcon : EyeOffIcon}
+      {status ? (
+        <VStack>
+          <Text>{status.message}</Text>
+          {status.status === 'success' ? (
+            <Link href='/'>{t('Home')}</Link>
+          ) : (
+            <Link href='/(auth)/reset-password'>{t('try again')}</Link>
+          )}
+        </VStack>
+      ) : (
+        <VStack className='w-full'>
+          <VStack space='xl' className='w-full'>
+            <FormControl isInvalid={!!errors.password} isDisabled={loading}>
+              <FormControlLabel>
+                <FormControlLabelText>{t('password')}</FormControlLabelText>
+              </FormControlLabel>
+              <Controller
+                defaultValue=''
+                name='password'
+                control={control}
+                rules={{
+                  validate: async (value) => {
+                    try {
+                      await schema.parseAsync({
+                        password: value,
+                      })
+                      return true
+                    } catch (error: any) {
+                      return error.message
+                    }
+                  },
+                }}
+                render={({ field: { onChange, onBlur, value } }) => (
+                  <Input>
+                    <InputField
+                      className='text-sm'
+                      placeholder={t('password')}
+                      value={value}
+                      onChangeText={onChange}
+                      onBlur={onBlur}
+                      onSubmitEditing={handleKeyPress}
+                      returnKeyType='done'
+                      type={showPassword ? 'text' : 'password'}
                     />
-                  </InputSlot>
-                </Input>
-              )}
-            />
-            <FormControlError>
-              <FormControlErrorIcon size='sm' as={AlertTriangle} />
-              <FormControlErrorText>
-                {t(errors?.confirmPassword?.message || '')}
-              </FormControlErrorText>
-            </FormControlError>
-          </FormControl>
-        </VStack>
+                    <InputSlot onPress={handleState} className='px-3'>
+                      <InputIcon as={showPassword ? EyeIcon : EyeOffIcon} />
+                    </InputSlot>
+                  </Input>
+                )}
+              />
+              <FormControlError>
+                <FormControlErrorIcon size='sm' as={AlertTriangle} />
+                <FormControlErrorText>
+                  {t(errors?.password?.message || '')}
+                </FormControlErrorText>
+              </FormControlError>
+            </FormControl>
 
-        <VStack className='w-full my-7' space='lg'>
-          <Button className='w-full' onPress={handleSubmit(onSubmit)}>
-            <ButtonText className='font-medium'>{t('sign up')}</ButtonText>
-          </Button>
-          {/* <Button
-            variant='outline'
-            action='secondary'
-            className='w-full gap-1'
-            onPress={() => {}}
-          >
-            <ButtonText className='font-medium'>
-              Continue with Google
-            </ButtonText>
-            <ButtonIcon as={GoogleIcon} />
-          </Button> */}
+            <FormControl
+              isInvalid={!!errors.confirmPassword}
+              isDisabled={loading}
+            >
+              <FormControlLabel>
+                <FormControlLabelText>
+                  {t('confirm password')}
+                </FormControlLabelText>
+              </FormControlLabel>
+              <Controller
+                defaultValue=''
+                name='confirmPassword'
+                control={control}
+                rules={{
+                  validate: async (value) => {
+                    try {
+                      await schema.parseAsync({
+                        password: value,
+                      })
+                      return true
+                    } catch (error: any) {
+                      return error.message
+                    }
+                  },
+                }}
+                render={({ field: { onChange, onBlur, value } }) => (
+                  <Input>
+                    <InputField
+                      placeholder={t('confirm password')}
+                      className='text-sm'
+                      value={value}
+                      onChangeText={onChange}
+                      onBlur={onBlur}
+                      onSubmitEditing={handleKeyPress}
+                      returnKeyType='done'
+                      type={showConfirmPassword ? 'text' : 'password'}
+                    />
+
+                    <InputSlot onPress={handleConfirmPwState} className='px-3'>
+                      <InputIcon
+                        as={showConfirmPassword ? EyeIcon : EyeOffIcon}
+                      />
+                    </InputSlot>
+                  </Input>
+                )}
+              />
+              <FormControlError>
+                <FormControlErrorIcon size='sm' as={AlertTriangle} />
+                <FormControlErrorText>
+                  {t(errors?.confirmPassword?.message || '')}
+                </FormControlErrorText>
+              </FormControlError>
+            </FormControl>
+          </VStack>
+
+          <VStack className='w-full my-7' space='lg'>
+            <Button
+              className='w-full'
+              onPress={handleSubmit(onSubmit)}
+              disabled={loading}
+            >
+              {loading ? <ButtonSpinner /> : null}
+              <ButtonText className='font-medium'>
+                {loading ? t('updating password...') : t('update password')}
+              </ButtonText>
+            </Button>
+          </VStack>
         </VStack>
-        <HStack className='self-center' space='sm'>
-          <Text size='md'>{t('already have an account?')}</Text>
-          <Link href='/login'>
-            <LinkText className='font-medium text-primary-700' size='md'>
-              {t('log in')}
-            </LinkText>
-          </Link>
-        </HStack>
-      </VStack>
+      )}
     </VStack>
   )
 }
